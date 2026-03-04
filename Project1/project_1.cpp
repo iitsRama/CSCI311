eca5182b84d5400ee08d2d8a96de35e0c6ec26cc
Amar <amarbulic@gmail.com>
2026-03-04 01:00:33 -0800

    Final cleanup of comments and functions. Data is good. Final Final edit.

--- Project1/project_1.cpp
+++ Project1/project_1.cpp
@@ -22,6 +22,7 @@ bool isSorted(const vector<int> &v)
             return false;
         }
     }
+
     return true;
 }
 
@@ -35,10 +36,12 @@ void bubbleSort(vector<int> &v)
 
     while(!sorted)
     {
+        // Assume sorted = true, then if belong set to false so while loop iterates
         sorted = true;
 
         for(int i = 1; i < v.size(); i++)
         {
+            // If last index > current index, swap them
             if(v[i-1] > v[i])
             {
                 int temp = v[i-1];
@@ -55,8 +58,10 @@ void insertionSort(vector<int> &v)
 {
     if (v.size() < 2) { return; }
 
+    // Increment inner loop start index, this is what separates insertion sort from bubble sort.
     for(int i = 1; i < v.size(); i++)
     {
+        // If current index < previous index, swap current index left. 
         for(int j = i; j > 0 && v[j] < v[j-1]; j--)
         {
             int temp = v[j-1];
@@ -71,9 +76,12 @@ void selectionSort(vector<int> &v)
 {
     if (v.size() < 2) { return; }
 
+    // Increment inner loops start index.
     for(int i = 0; i < v.size() - 1; i++)
     {
         int uMin = i;
+        
+        // Inner loop will find smallest value.
         for(int j = i + 1; j < v.size(); j++)
         {
             if(v[j] < v[uMin])
@@ -81,99 +89,84 @@ void selectionSort(vector<int> &v)
                 uMin = j;
             }
         }
+        
+        // Set smallest value to v[i], or the correct ascending order place.
         int temp = v[i];
         v[i] = v[uMin];
         v[uMin] = temp;
     }
 }
 
-// Quick sort, a pivot point is chosen, the first element of the list here, and then sorts remaining values into two vectors:
-// Vector A is values less than or equal to that pivot value
-// Vector B is values greater than that pivot value
-// then Quick sort is recursively called on each of those vectors
-// Continues until A and B are sorted in ascending order, then our pivot value will be our median value, so we concotenate v = A + pivot + B.
+// Quick sort, index 0 is pivot, int i and j chosen as [1] and [size - 1], increment i and decrement j until i > pivot, j < pivot, swap those values, repeat.
+// when i > j, break out of above loop, swap pivot with j, then recursive call to sort the left and right halves of the list.
+void quickSortHelper(vector<int>&v, int low, int high)
+{
+    // Base case, when indices reach each other as start values
+    if (low >= high) return;
+
+    int pivot = v[low];
+    int i = low + 1;
+    int j = high;
+
+    while (true)
+    {
+        // i <= j ensures no out of bounds
+        // increment i and check against pivot value, stop when > pivot
+        // increment j and check agains tpivot value, stop when <= pivot
+        while (i <= j && v[i] <= pivot) { i++; }
+        while (i <= j && v[j] > pivot) { j--; }
+
+        // When indices pass each other, or when i passes j, leave loop
+        if(i > j) { break; }
+
+        // Swap v[i] & v[j]
+        int temp = v[i];
+        v[i] = v[j];
+        v[j] = temp;
+    }
+
+    // Swap pivot with j, j ends at last element <= pivot
+    int temp = v[low];
+    v[low] = v[j];
+    v[j] = temp;
+
+    // Recursion on sub arrays, left and right from pivot
+    quickSortHelper(v, low, j - 1);
+    quickSortHelper(v, j + 1, high);
+}
+
+// Quick sort container that allows us to use function pointers below. Calls quicksorthelper.
 void quickSort(vector<int> &v)
 {
-    if (v.size() < 2) { return; }
-
-    int pivot = v[0];
-    vector<int> A;
-    vector<int> B;
-
-    for(int i = 1; i < v.size(); i++)
-    {
-        if(v[i] <= pivot)
-        {
-            A.push_back(v[i]);
-        }
-        else
-        {
-            B.push_back(v[i]);
-        }
-    }
-
-    quickSort(A);
-    quickSort(B);
-
-    v = A;
-    v.push_back(pivot);
-    for(int j = 0; j < B.size(); j++)
-    {
-        v.push_back(B[j]);
-    }
+    int low = 0;
+    int high = v.size() - 1;
+    quickSortHelper(v, low, high);
 }
 
-// Same as above quick sort, but our pivot value is the median value of the entire list, this code only works when the list is sorted in ascending order
-// Specifically to test the best case scenario of quick sort.
+// Same as above quick sort, but our pivot value is the median value of the entire list.
+void bestQuickSortHelper(vector<int> &v, int low, int high)
+{
+    // Base case, when indices reach each other as start values
+    if (low >= high) return;
+
+    // Median value of list, only works if list is sorted.
+    int mid = (low + high) / 2;
+
+    // Swap mid value to first value of list. This allows us to avoid creating an overloaded function of runTests below
+    // and instead allows us to call our quickSortHelper above.
+    int tmp = v[low];
+    v[low] = v[mid];
+    v[mid] = tmp;
+
+    quickSortHelper(v, low, high);
+}
+
 void bestQuickSort(vector<int> &v)
 {
-    if (v.size() < 2) { return; }
-
-    int pivot = v[v.size() / 2];
-    vector<int> A;
-    vector<int> B;
-
-    for(int i = 0; i < v.size(); i++)
-    {
-        if(v[i] == pivot) { continue; };
-        if(v[i] <= pivot)
-        {
-            A.push_back(v[i]);
-        }
-        else
-        {
-            B.push_back(v[i]);
-        }
-    }
-
-    bestQuickSort(A);
-    bestQuickSort(B);
-
-    v = A;
-    v.push_back(pivot);
-    for(int j = 0; j < B.size(); j++)
-    {
-        v.push_back(B[j]);
-    }
+    int low = 0;
+    int high = v.size() - 1;
+    bestQuickSortHelper(v, low, high);
 }
-
-// Prints out entire vector, used for debugging.
-// void printVector(const vector<int> &v)
-// {
-//     std:: cout << v.size() << std::endl;
-//     std::cout << "{";
-
-//     if(v.size() == 0)
-//     {
-//         std::cout << "}" << std::endl;
-//         return;
-//     }
-//     for(int i = 0; i < v.size() - 1; i++)
-//     {
-//         std::cout << v[i] << ", ";
-//     }
-//     std::cout << v[v.size() - 1] << "}" << std::endl;
-// }
 
 // Calculate the sample standard deviation of a vector of doubles
 // v - const vector<double> - a vector of doubles
@@ -196,7 +189,7 @@ double sampleSD(const vector<double> &v)
 }
 
 // Measures time taken to sort a vector.
-// void (*sortFunc)(std::vector<int>&) is a function to a pointer which returns void and takes a reference to a vector of integers as an argument
+// void (*sortFunc)(std::vector<int>&) is a pointer to a function which returns void and takes a reference to a vector of integers as an argument
 // This allows us to write just one timing function, and pass different sorting functions to it.
 double timeSort(vector<int> &v, void (*sortFunc)(std::vector<int>&))
 {
@@ -246,8 +239,8 @@ vector<double> calculateTime(const vector<double> &v)
     return timeOutliers;
 }
 
+// Processes data
 // passing list by value makes a copy, lets me modify list all I want
-// Processes data and optionally prints, commented out as not the desired output from assignment, useful for debugging
 // sortName, file used to organize data in their respective .csv files
 // list is a vector of vectors
 // same function pointer as above, allows use of separate sorts.
@@ -257,18 +250,18 @@ void processData(std::string sortName, vector<vector<int>> list, std::ofstream &
     vector<double> timeOutliers;
     bool sorted = true;
 
+    // Sort all the vector in list, calculate the time it took, push that time into an array, and write to .csv
     for(int i = 0; i < list.size(); i++)
     {
         double timeValue = timeSort(list[i], sortFunc);
         timeValues.push_back(timeValue);
         file << sortName << "," << list[i].size() << "," << timeValue << std::endl;
-        sorted = sorted && isSorted(list[i]);
     }
-
-    timeOutliers = calculateTime(timeValues);
 }
 
-// Overloaded version of above function to accommodate base output to command line that assignment desires.
+// Overloaded version of above function to accommodate base output to command line that assignment desires. 
+// Outputs to console based on 10 arrays of size 100
+// Outputs Min, Mean, StdDev, and Max time taken
 void processData(std::string sortName, vector<vector<int>> list, void (*sortFunc)(std::vector<int>&))
 {
     vector<double> timeValues;
@@ -343,6 +336,8 @@ vector<vector<int>> reverseSortedVector(int size, int low, int high, int amount)
 
     for(int i = 0; i < amount; i++)
     {
+        // Set start value as [high - (high - low - (size - 1)) : high] ensures reverse sorted vector that is in bounds.
+        // Ex.) if size = 1000, high value = 10000, range is [999 : 10000] -> rand % 9002 = 0, 1, 2, ... , 9001; high - 9001 = 999
         int start = high - rand() % (high - low - (size - 1) + 1);
         for(int j = 0; j < size; j++)
         {
@@ -357,38 +352,42 @@ vector<vector<int>> reverseSortedVector(int size, int low, int high, int amount)
 // Generate a specified amount of vectors ranging from [low, high]
 // Special case for the worst case run time of selection sort
 // vector will be ascending even numbers until size / 2 is reached, then will swap to descending odd numbers for the remaining space
+// Passed int high not used, as it would require an overload of one of the data functions below. This keeps it inline with the other 
+// vector generating functions.
 vector<vector<int>> worstSelectionVector(int size, int low, int high, int amount)
 {
     vector<vector<int>> list;
     list.reserve(amount);
 
-    for(int t = 0; t < amount; t++)
+    for(int i = 0; i < amount; i++)
     {
-        int hi = 100000 + rand() % (250000 - 100000 + 1);
-
         int half = size / 2;
 
-        int difference = hi / half;
-        if (difference < 2) difference = 2;
+        // Find step value, this will enable the pattern ascending even values -> descending odd values, within range, evenly spaced.
+        int difference = high / half;
+        
+        // Ensure step is an even number
         if (difference % 2 != 0) difference++; 
 
         vector<int> v(size);
 
-        int even = low;
-        int odd  = hi - 1;
+        int ascending = low;
+
+        // Starting at high - 1 allows us to keep difference at an even value, and will always give us an odd result when subtracting.
+        int descending  = high - 1;
 
         // first half: ascending evens
-        for(int i = 0; i < size/2; i++)
+        for(int j = 0; j < size/2; j++)
         {
-            v[i] = even;
-            even += difference;
+            v[j] = ascending;
+            ascending += difference;
         }
 
         // second half: descending odds
-        for(int i = size/2; i < size; i++)
+        for(int k = size/2; k < size; k++)
         {
-            v[i] = odd;
-            odd -= difference;
+            v[k] = descending;
+            descending -= difference;
         }
 
         list.push_back(v);
@@ -402,6 +401,7 @@ vector<vector<int>> worstSelectionVector(int size, int low, int high, int amount
 void runTests(vector<vector<int>> &v, int low, int high, int amount, std::ofstream &file,
                 void (*sortFunc)(std::vector<int>&), std::string sortName, char caseName)
 {
+    // All list sizes tested.
     std::vector<int> sizes = {100, 200, 400, 800, 1600, 3200, 6400, 12800};
 
     for(int i = 0; i < sizes.size(); i++)
@@ -434,8 +434,7 @@ void runTests(vector<vector<int>> &v, int low, int high, int amount, std::ofstre
 }
 
 // Overloaded version of above function, allows us to pass the same arguments except for the file, so that we can output the base case to console
-// without writing to a file.
-// May change to write to a separate file.
+// without writing to a file. Used for question 2.1
 void runTests(vector<vector<int>> &v, int startValue, int endValue, int amount,
                 void (*sortFunc)(std::vector<int>&), std::string sortName, char caseName)
 {
@@ -455,9 +454,9 @@ void runTests(vector<vector<int>> &v, int startValue, int endValue, int amount,
 
 int main()
 {
-    std::ofstream worstFile("data/worst0.csv");
-    std::ofstream averageFile("data/average0.csv");
-    std::ofstream bestFile("data/best0.csv");
+    std::ofstream worstFile("data/worst.csv");
+    std::ofstream averageFile("data/average.csv");
+    std::ofstream bestFile("data/best.csv");
 
     srand(time(NULL));
 
@@ -496,3 +495,21 @@ int main()
 
     return 0;
 };
+
+// Prints out entire vector, used for debugging.
+// void printVector(const vector<int> &v)
+// {
+//     std:: cout << v.size() << std::endl;
+//     std::cout << "{";
+
+//     if(v.size() == 0)
+//     {
+//         std::cout << "}" << std::endl;
+//         return;
+//     }
+//     for(int i = 0; i < v.size() - 1; i++)
+//     {
+//         std::cout << v[i] << ", ";
+//     }
+//     std::cout << v[v.size() - 1] << "}" << std::endl;
+// }
