f7e014415afbadabf85de5fcc233e167d3a10b2c
Amar <amarbulic@gmail.com>
2026-03-16 23:51:01 -0700

    Functions added: Constructor, search, min, max, balance factor, height. Working on insertValue

--- Lab5/AVLTree.cpp
+++ Lab5/AVLTree.cpp
@@ -10,66 +10,107 @@ using namespace std;
 
 AVLTree::AVLTree()
 {
+  root = nullptr;
+  size = 0;
 }
 
 std::shared_ptr<AVLNode> AVLTree::getRoot()
 {
-  return nullptr;
+  return root;
 }
 
 int AVLTree::getSize()
 {
-  return -1;
+  return size;
 }
 
 std::shared_ptr<AVLNode> AVLTree::search(int val)
 {
-  return nullptr;
+  return search(root, val);
 }
 
 std::shared_ptr<AVLNode> AVLTree::search(std::shared_ptr<AVLNode> n, int val)
 {
+  if(n == nullptr) { return nullptr; }
+  if(n -> value == val) { return n; }
+
+  if(val <= n -> value)
+  {
+    return search(n -> left, val); 
+  }
+  else
+  {
+    return search(n -> right, val);
+  }
+
   return nullptr;
 }
 
 std::shared_ptr<AVLNode> AVLTree::minimum()
 {
-  return nullptr;
+  return minimum(root);
 }
 
 std::shared_ptr<AVLNode> AVLTree::minimum(std::shared_ptr<AVLNode> n)
 {
+  if(n == nullptr) { return nullptr; }
+
+  if(n -> left == nullptr) { return n; }
+  else
+  {
+    return minimum(n -> left);
+  }
+
   return nullptr;
 }
 
 std::shared_ptr<AVLNode> AVLTree::maximum()
 {
-  return nullptr;
+  return maximum(root);
 }
 
 std::shared_ptr<AVLNode> AVLTree::maximum(std::shared_ptr<AVLNode> n)
 {
+  if(n == nullptr) { return nullptr; }
+
+  if(n -> right == nullptr) { return n; }
+  else
+  {
+    return maximum(n -> right);
+  }
+
   return nullptr;
 }
 
 int getHeight(std::shared_ptr<AVLNode> n)
 {
-  return -1;
+  return n -> height;
 }
 
 int getBalanceFactor(std::shared_ptr<AVLNode> n)
 {
-  return -99;
+  return n -> balanceFactor;
 }
 
 void AVLTree::insertValue(int val)
 {
+  insertValue(root, val);
 }
 
-std::shared_ptr<AVLNode> AVLTree::insertValue(std::shared_ptr<AVLNode> n,
-                                              int val)
+std::shared_ptr<AVLNode> AVLTree::insertValue(std::shared_ptr<AVLNode> n, int val)
 {
-  return nullptr;
+  if(n == nullptr){
+    n = make_shared<AVLNode>(val);
+  }
+  else if(val <= n -> value)
+  {
+    return insertValue(n -> left, val);
+  }
+  else if(val > n -> value)
+  {
+    return insertValue(n -> right, val);
+  }
+  return n;
 }
 
 void AVLTree::deleteValue(int val)
